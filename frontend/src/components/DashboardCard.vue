f1f20b5ffe7eb6b2d415c86aee0c8afd88ceb721
Igor Sarwiński <igor.s@onet.pl>
2024-07-24 19:02:45 +0200

    zaimpletowanie dashboardu
    zaimpletowanie dashboardu

---
+++ frontend/src/components/DashboardCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  name: String,
+  buttonLabel: String,
+  background: String,
+  link: String,
+})
+</script>
+
+<template>
+  <router-link :to="link" class="dashboard__card">
+  <div class="dashboard__card-info">
+    <span class="dashboard__card-info-name"><p>{{ name }}</p></span>
+    <span class="dashboard__card-info-button"><p>{{ buttonLabel }}</p></span>
+  </div>
+    <img class="dashboard__card-background" :src="`src/assets/img/dashboard/${background}`" alt="{{background}}">
+  </router-link>
+</template>
--- frontend/src/views/DashboardPage.vue
+++ frontend/src/views/DashboardPage.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+import DashboardCard from '@/components/DashboardCard.vue'
 
+const items = ref([
+  { id: 1, name: 'Ruletka', link: '/roulette', buttonLabel: 'Graj!', background: 'ruletka.webp' },
+  { id: 2, name: 'Blackjack', link: '/blackjack', buttonLabel: 'Graj!', background: 'blackjack.png' },
+  { id: 3, name: 'Sloty', link: '/slots', buttonLabel: 'Graj!', background: 'slotsy.jpg' },
+  { id: 4, name: 'Ranking', link: '/ranking', buttonLabel: 'Zobacz!', background: 'rankingi.jpg' },
+])
 </script>
 
 <template>
-  <h1>Strona główna</h1>
+  <div class="dashboard">
+    <DashboardCard
+      v-for="item in items"
+      :key="item.id"
+      :name="item.name"
+      :buttonLabel="item.buttonLabel"
+      :background="item.background"
+      :link="item.link"
+    />
+  </div>
 </template>
-
-
